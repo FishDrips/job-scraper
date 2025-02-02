6696b56a0b8db90f16e31f6c8060806c47d9bee0
Your Name <you@example.com>
2025-02-02 20:58:22 +0000

    added webdriver and also added code to navigate to the job URL

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0'
+HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0'}
+JOB_URL = 'https://www.google.com/search?sca_esv=0dcf27f7239faca2&hl=en&biw=1920&bih=919&sxsrf=AHTn8zpOMIpVN_jzo6Z-Hnwe_mXuUbh_VA:1738527790314&q={0}&udm=8&fbs=ABzOT_CWdhQLP1FcmU5B0fn3xuWpA-dk4wpBWOGsoR7DG5zJBnRhW2_is7MhE2uHZXC2Kp-K878BjLmyCCstxJdBYRc-o0mVzMMruV8STjOF2eC0cseG_bdEktd62NlRqIcKaKVOxbXyt0CpXPFw1UJPPc36IJIiPzBiO5gknJVEIlBU36HtQMSC1aALsyOeuEfz4iZtvccQ&sa=X&ved=2ahUKEwjxxf7a6KWLAxU2RUEAHWkAHd4Qs6gLegQIExAB&jbr=sep:0'
+
+COOKIE = {
+    '__Secure-ENID': '25.SE=PEySymhdMwwzdrTzCz8swjB2KPQ3-_1ADdhZErPSc3xdgOxj04wB8-V_3LCRVDCUdzqgSfUpQoZl7YPx7I7yZNCKEpstzYX2lNPKxxOQWJvI6-HSlUshUXtyXcw26E65Mk1rDVSw_pfy8pyEG9anKPUWIkha0ZcB_wgJcb1ZzdlRiTAeBJNwBRdz1urjQeCxGPG96wcG1vsbkM2EvML054_DuBlBHVexCdDPAXOmHfg',
+    'AEC': 'AVcja2fcXilyrdvYJbS0c9InN_xRpX106uRPbLY0v-68GR-N-D0m5WWzNg',
+    'SOCS': 'CAESHAgCEhJnd3NfMjAyNTAxMjktMF9SQzEaAmVuIAEaBgiA6_q8Bg'
+}
+
+
+def add_cookie(driver):
+    for k, v in COOKIE.items():
+        driver.add_cookie({'name': f'{k}', 'value': f'{v}'})
 
 def run():
-    pass
+    driver = webdriver.Firefox()
+    driver.get(JOB_URL.format("senior+sofware+engineer"))
+    add_cookie(driver)
+    driver.refresh()
+    
+    # soup = BeautifulSoup(response.content)
+    # print(response.content)
 
 if __name__ == '__main__':
     run()
