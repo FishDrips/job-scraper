1db2b8cb499f145aea1462f1e4ec6a73b0142e6c
Your Name <you@example.com>
2025-02-02 20:21:47 +0000

    init project

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0'
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
